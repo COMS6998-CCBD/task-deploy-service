3514f2c34bc152c970d116258dce9e247fa1e955
Akhil Ravipati <akhil.ravipati.o@gmail.com>
2021-12-01 23:26:04 -0500

    Add task name

--- service/deploy_service/task_deploy_request.py
+++ service/deploy_service/task_deploy_request.py
@@ -2,9 +2,10 @@ from typing import List
 from constants import *
 
 class TaskDeployRequest:
-    def __init__(self, user_id: str, task_id: str, s3_bucket: str, source_s3_prefix: str, destination_s3_prefix: str, command: str, linux_dependencies: List[str]):
+    def __init__(self, user_id: str, task_id: str, task_name: str, s3_bucket: str, source_s3_prefix: str, destination_s3_prefix: str, command: str, linux_dependencies: List[str]):
         self.user_id = user_id
         self.task_id = task_id
+        self.task_name = task_name
         self.s3_bucket = s3_bucket
         self.source_s3_prefix = source_s3_prefix
         self.destination_s3_prefix = destination_s3_prefix
